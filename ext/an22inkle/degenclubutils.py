9dafbc04f7196b608404ccfdd0d782bcaea63cb3
AnotherTwinkle <anothersparklingstar@gmail.com>
2021-12-26 22:02:01 +0600

    A special cog for VN stuff

---
+++ ext/an22inkle/degenclubutils.py
@@ -0,0 +1,45 @@
+from discord.ext import commands
+import discord
+import urllib
+
+class DegenClubUtils(commands.Cog):
+	'''A set of utilites for the degenclub VN'''
+	def __init__(self, bot):
+		self.bot = bot
+		self.channels = {}
+		self.dialouge_safe_prefix = 'dg '
+		self.action_safe_prefix = 'ac '
+		# Note, both of the prefixes must be 2 letters long plus a space (LEN 3), I'm sorry.
+
+	@commands.command(hidden= True, aliases= ['img'])
+	async def image(self, ctx, *, name : str):
+		name = urllib.parse.quote(name if name.lower().endswith('.png') else name + '.png')
+		return await ctx.send(f'https://raw.githubusercontent.com/AnotherTwinkle/degenclub/master/game/images/{name}')
+
+	@commands.command(hidden= True, name='reflectionchannel', aliases= ['rfc', 'rc'])
+	async def _set_reflection_channel(self,  ctx, channel: discord.Channel):
+		self.channels[ctx.channel] = channel
+		await ctx.send('ok.')
+
+	@commands.Cog.listener('on_message')
+	async def story_constructor_listener(self, message):
+		if message.channel not in self.channels or message.author.bot: 
+			return
+
+		if (not message.content.startswith((self.dialouge_safe_prefix, self.action_safe_prefix))):j
+			return
+
+		l = message.content[3:].split(' ')  # Remove the safe prefix and make each word part of a list
+		if message.content.startswith(self.dialouge_safe_prefix):
+			l[0] = f'**{l[0]}**' # Bold the first word, this should be the actor of the dialouge
+
+		content = ' '.join(l)
+		if message.content.startswith(self.action_safe_prefix):
+			content = f'**{content}**' # Bold the whole fucking thing
+
+		await self.channels[message.channel].send(content)
+
+
+def setup(bot):
+	bot.add_cog(DegenClubUtils(bot))
+
